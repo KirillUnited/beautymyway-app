f4aa46c8332535c27d728ece42c2c217e78e24f0
k.dubov <example@example.com>
2023-10-24 13:46:09 +0300

    Update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,18 @@ import './globals.css'
 import { mulish } from './fonts';
 
 export const metadata: Metadata = {
+	metadataBase: new URL('https://site.com'),
 	title: 'Артур Сугако теперь и в Барселоне!',
 	description: 'Красота – это свобода! Так было сто лет назад – все также и сегодня. Изменились только технологии.',
+	keywords: ['Салон красоты', 'Барселона', 'Процедуры'],
+	openGraph: {
+		type: 'website',
+		locale: 'en_IE',
+		url: 'https://www.url.ie/a',
+		title: 'OG Title',
+		description: 'OG Decription',
+		siteName: 'Example',
+	},
 }
 
 export default function RootLayout({

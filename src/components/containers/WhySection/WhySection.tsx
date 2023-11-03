4bae9fdde2e5a2c87cde53b2369b218ae6de97b8
k.dubov <example@example.com>
2023-11-03 18:56:06 +0300

    Update why section

--- src/components/containers/WhySection/WhySection.tsx
+++ src/components/containers/WhySection/WhySection.tsx
@@ -4,22 +4,24 @@ import styles from './WhySection.module.scss';
 export default function WhySection(): React.JSX.Element {
     return (
         <section className={styles.root}>
-            <div className={styles.wrapper}>
-                <h2 className={styles.title}>
-                    Почему мы?
-                </h2>
-                <div className={styles.content}>
-                    <p className={styles['content-item']}>15
-                        <span className="text-lg font-light leading-7">Лет опыта</span>
-                    </p>
-                    <p className={styles['content-item']}>92,880
-                        <span className="text-lg font-light leading-7">Проведенных процедур</span>
-                    </p>
-                    <p className={styles['content-item']}>30,960
-                        <span className="text-lg font-light leading-7">Клиентов</span>
-                    </p>
-                </div>
-            </div >
+            <div className='container'>
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>
+                        Почему мы?
+                    </h2>
+                    <div className={styles.content}>
+                        <p className={styles['content-item']}>15
+                            <span className="text-base lg:text-lg font-light leading-7">Лет опыта</span>
+                        </p>
+                        <p className={styles['content-item']}>92,880
+                            <span className="text-base lg:text-lg font-light leading-7">Проведенных процедур</span>
+                        </p>
+                        <p className={styles['content-item']}>30,960
+                            <span className="text-base lg:text-lg font-light leading-7">Клиентов</span>
+                        </p>
+                    </div>
+                </div >
+            </div>
         </section >
     )
 }

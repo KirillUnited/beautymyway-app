a71095dcc1074a8e227640ca9063f1aafa3513db
k.dubov <example@example.com>
2023-11-13 12:03:05 +0300

    Update info

--- src/app/containers/InfoSection/InfoSection.tsx
+++ src/app/containers/InfoSection/InfoSection.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@/components/Button/Button';
 import Contacts from '@/components/Contacts/Contacts';
 import styles from './InfoSection.module.scss';
 
@@ -7,10 +6,8 @@ export default function InfoSection() {
     return (
         <section className={styles.root}>
             <div className={styles.wrapper}>
-                <h2 className={styles.title}>Dr. Artur Sugako Clinic теперь и в Барселоне!</h2>
-                <p className={styles.description}>Чтобы получить консультацию или записаться, пожалуйста, свяжитесь по указанному на сайте контактному номеру. Информацию о ценах на услуги можно найти ниже.</p>
-                <Contacts className={styles.contacts} tel={`+34 601 500 379`} address={'Barcelona, Carrer de Bertrand i Serra, 12'} time={'Пн – Сб: 10:00 – 20:00'} />
-                <Button variant='primary' className={styles.button}>Услуги</Button>
+                <p className={styles.description}>Чтобы получить консультацию или записаться, пожалуйста, свяжитесь по указанному на сайте контактному номеру.</p>
+                <Contacts className={styles.contacts} tel={`[+34 643 942 528, +34 643 939 409]`} address={'Valencia'} time={'Пн – Сб: 10:00 – 20:00'} />
             </div>
         </section>
     )

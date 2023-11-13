3334bcff609c9dd3f5b4f1308d1ce57b65e77106
k.dubov <example@example.com>
2023-11-13 13:11:32 +0300

    Remove why section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,6 @@ export default function Home() {
 			<AboutSection />
 			<ProductsSection />
 			<TeamSection />
-			<WhySection />
 			<HitSection />
 			<TestimonialsSection />
 			<FeedsSection />

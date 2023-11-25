0915ca8f4f0bacc8800fe4f29c5f2ec3ab85285d
k.dubov <example@example.com>
2023-11-25 20:15:33 +0300

    Update contacts

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -5,33 +5,39 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export default function Socials() {
+export default function Socials({items} : any) {
+    const elements: any = {
+        'instagram': (values: string) => {
+            return (
+                <a href={values} target='_blank' className={`flex gap-4 items-center`}>
+                    <FontAwesomeIcon icon={faInstagram} className='w-5 h-5' />
+                </a>
+            )
+        },
+        'telegram': (values: string) => {
+            return (
+                <a href={values} target='_blank' className={`flex gap-4 items-center`}>
+                    <FontAwesomeIcon icon={faTelegram} className='w-5 h-5' />
+                </a>
+            )
+        },
+        'whatsapp': (values: string) => {
+            return (
+                <a href={values} target='_blank' className={`flex gap-4 items-center`}>
+                    <FontAwesomeIcon icon={faWhatsapp} className='w-5 h-5' />
+                </a>
+            )
+        }
+    };
+
     return (
         <ul className="socials flex justify-center items-center gap-3 lg:gap-4">
             {
-                Object.entries(CONTACTS.socials).map(([keys, values], index) => {
+                Object.entries(items).map(([keys, values], index) => {
                     return (
                         <li key={index}>
                             {
-                                keys === 'instagram'
-                                &&
-                                <a href={values} target='_blank' className={`flex gap-4 items-center`}>
-                                    <FontAwesomeIcon icon={faInstagram} className='w-5 h-5' />
-                                </a>
-                            }
-                            {
-                                keys === 'telegram'
-                                &&
-                                <a href={values} target='_blank' className={`flex gap-4 items-center`}>
-                                    <FontAwesomeIcon icon={faTelegram} className='w-5 h-5' />
-                                </a>
-                            }
-                            {
-                                keys === 'whatsapp'
-                                &&
-                                <a href={values} target='_blank' className={`flex gap-4 items-center`}>
-                                    <FontAwesomeIcon icon={faWhatsapp} className='w-5 h-5' />
-                                </a>
+                                elements[keys](values)
                             }
                         </li>
                     )

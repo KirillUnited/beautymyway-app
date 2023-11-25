26185a54daa160a04d8081fb1fb08dc4ea2678c5
k.dubov <example@example.com>
2023-11-25 15:43:24 +0300

    Update hero section

--- src/app/containers/HeroSection/HeroSection.tsx
+++ src/app/containers/HeroSection/HeroSection.tsx
@@ -16,7 +16,7 @@ export default function HeroSection(): React.JSX.Element {
             className={`${styles['bg-image']}`}
         />
             <div className="container">
-                <div className={`${styles.inner} relative grid grid-cols-1 md:grid-cols-2 justify-center items-center lg:gap-14 max-w-5xl mx-auto`}>
+                <div className={`${styles.inner}`}>
                     <div className='flex flex-col items-center py-5 z-10'>
                         <div className={styles.text}>
                             <p className="font-normal">Свобода - это красота.</p>

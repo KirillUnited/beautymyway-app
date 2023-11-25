1d5f862fff948b6541c35fd30aa80ee824690bc7
k.dubov <example@example.com>
2023-11-25 19:08:24 +0300

    Add ButtonLink

--- src/app/containers/HitSection/HitSection.tsx
+++ src/app/containers/HitSection/HitSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ImageSectionLayout from '../ImageSectionLayout';
 import styles from './HitSection.module.scss';
-import Button from '@/components/Button/Button';
+import { ButtonLink } from '@/components/Button/Button';
 
 export default function HitSection() {
     return (
@@ -14,7 +14,7 @@ export default function HitSection() {
             <p>
             Фотоомоложение представляет собой медицинскую процедуру, направленную на предотвращение и устранение признаков фотостарения кожи. Эффективная и неинвазивная, эта технология осуществляется с использованием высокоинтенсивных источников света (IPL), представляя собой одно из наиболее перспективных направлений в области эстетической медицины.
             </p>
-            <Button variant='primary' className='self-center lg:self-start mt-4 lg:mt-auto'>Детальнее о процедуре</Button>
+            <ButtonLink href={'/posts/fotoomolozhenie-ipl'} variant='primary' className='self-center lg:self-start mt-4 lg:mt-auto'>Детальнее о процедуре</ButtonLink>
         </ImageSectionLayout>
     )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './Button.module.scss';
-import { ButtonProps } from './Button.props';
+import { ButtonLinkProps, ButtonProps } from './Button.props';
 import cn from 'classnames';
+import Link from 'next/link';
 
 export default function Button({ variant, children, className, ...props }: ButtonProps): React.JSX.Element {
     return (
@@ -18,3 +19,19 @@ export default function Button({ variant, children, className, ...props }: Butto
         </button>
     )
 }
+
+export function ButtonLink({ variant, children, className, ...props }: ButtonLinkProps): React.JSX.Element {
+    return (
+        <Link className={cn(styles.default,
+            className,
+            {
+                [styles.primary]: variant === 'primary',
+                [styles.transparent]: variant === 'transparent'
+            }
+        )}
+        {...props}
+        >
+            {children}
+        </Link>
+    )
+} 

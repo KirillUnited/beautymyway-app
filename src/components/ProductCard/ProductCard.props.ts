40762fbc53f8b119b79fb88d4dcaeb9ff21e85c3
k.dubov <example@example.com>
2023-12-18 14:40:43 +0300

    refact: product card props

---
+++ src/components/ProductCard/ProductCard.props.ts
@@ -0,0 +1,6 @@
+export interface IProductCardProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+    image: string,
+    title: string,
+    variant?: 'featured' | 'default',
+    description?: string
+}

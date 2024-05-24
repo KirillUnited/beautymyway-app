89b1f23b492fd4124763dcea446ea5cff9aa75d6
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-05-24 14:17:58 +0300

    Fix nested layout issue

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,4 @@
 import type { Metadata } from 'next';
-import '@mantine/core/styles.css';
-import '@/styles/globals.scss';
-import { mulish, futura } from '@/config/fonts';
-import { theme } from '@/config/theme';
-import { MantineProvider, ColorSchemeScript, createTheme } from '@mantine/core';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { NextIntlClientProvider } from 'next-intl';
@@ -16,17 +11,6 @@ export const metadata: Metadata = {
 		template: `%s | Beautymyway ➤ Професиональный косметолог в Валенсии`,
 	},
 	description: 'Beautymyway ➤ косметолог в Валенсии⭐ Профессионально помогаю обрести красоту и здоровье! ✅ ➤ Опыт более 15 лет!',
-	keywords: ['Массаж', 'Endosphere', 'Emsculpt', 'Dion laser', 'БАД', 'питание', 'спорт', 'косметолог', 'врач', 'ботокс', 'мезотерапия', 'биоревитализация', 'Myway', 'Beauty', 'BeautyMyway', 'липолитики', 'карбокситерапия', 'чистка', 'лицо', 'кожа', 'тело', 'красота', 'здоровье', 'гармония', 'психология'],
-	openGraph: {
-		type: 'website',
-		locale: 'en_IE',
-		url: 'https://beautymyway.es',
-		title: 'Beautymyway ➤ Професиональный косметолог в Валенсии',
-		description: 'Beautymyway ➤ косметолог в Валенсии⭐ Профессионально помогаю обрести красоту и здоровье! ✅ ➤ Опыт более 15 лет!',
-		siteName: 'beautymyway',
-		images: '/apple-icon.png'
-	},
-	themeColor: '#2b3632',
 };
 
 export default async function LocaleLayout({
@@ -39,23 +23,14 @@ export default async function LocaleLayout({
 	const messages = await getMessages();
 
 	return (
-		<html lang={params.locale}>
-			<head>
-				<ColorSchemeScript />
-			</head>
-			<body className={`${mulish.className} ${futura.variable} text-primary bg-foreground`}>
-				<MantineProvider theme={theme}>
-					<NextIntlClientProvider messages={messages}>
-						<div className='wrapper flex flex-col min-h-screen'>
-							<Header></Header>
-							<main className="content flex-1">
-								{children}
-							</main>
-							<Footer></Footer>
-						</div>
-					</NextIntlClientProvider>
-				</MantineProvider>
-			</body>
-		</html>
+		<NextIntlClientProvider messages={messages}>
+			<div className='wrapper flex flex-col min-h-screen'>
+				<Header></Header>
+				<main className="content flex-1">
+					{children}
+				</main>
+				<Footer></Footer>
+			</div>
+		</NextIntlClientProvider>
 	)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,52 +2,47 @@ import type { Metadata } from 'next';
 import '@mantine/core/styles.css';
 import '@/styles/globals.scss';
 import { mulish, futura } from '@/config/fonts';
-import {theme} from '@/config/theme';
-import { MantineProvider, ColorSchemeScript,createTheme } from '@mantine/core';
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
+import { theme } from '@/config/theme';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 
-// export const metadata: Metadata = {
-// 	metadataBase: new URL('https://beautymyway.es'),
-// 	title: {
-// 		default: 'Beautymyway ➤ Професиональный косметолог в Валенсии',
-// 		template: `%s | Beautymyway ➤ Професиональный косметолог в Валенсии`,
-// 	},
-// 	description: 'Beautymyway ➤ косметолог в Валенсии⭐ Профессионально помогаю обрести красоту и здоровье! ✅ ➤ Опыт более 15 лет!',
-// 	keywords: ['Массаж', 'Endosphere', 'Emsculpt', 'Dion laser', 'БАД', 'питание', 'спорт', 'косметолог', 'врач', 'ботокс', 'мезотерапия', 'биоревитализация', 'Myway', 'Beauty', 'BeautyMyway', 'липолитики', 'карбокситерапия', 'чистка', 'лицо', 'кожа', 'тело', 'красота', 'здоровье', 'гармония', 'психология'],
-// 	openGraph: {
-// 		type: 'website',
-// 		locale: 'en_IE',
-// 		url: 'https://beautymyway.es',
-// 		title: 'Beautymyway ➤ Професиональный косметолог в Валенсии',
-// 		description: 'Beautymyway ➤ косметолог в Валенсии⭐ Профессионально помогаю обрести красоту и здоровье! ✅ ➤ Опыт более 15 лет!',
-// 		siteName: 'beautymyway',
-// 		images: '/apple-icon.png'
-// 	},
-// 	themeColor: '#2b3632',
-// };
+export const metadata: Metadata = {
+	metadataBase: new URL('https://beautymyway.es'),
+	title: {
+		default: 'Beautymyway ➤ Професиональный косметолог в Валенсии',
+		template: `%s | Beautymyway ➤ Професиональный косметолог в Валенсии`,
+	},
+	description: 'Beautymyway ➤ косметолог в Валенсии⭐ Профессионально помогаю обрести красоту и здоровье! ✅ ➤ Опыт более 15 лет!',
+	keywords: ['Массаж', 'Endosphere', 'Emsculpt', 'Dion laser', 'БАД', 'питание', 'спорт', 'косметолог', 'врач', 'ботокс', 'мезотерапия', 'биоревитализация', 'Myway', 'Beauty', 'BeautyMyway', 'липолитики', 'карбокситерапия', 'чистка', 'лицо', 'кожа', 'тело', 'красота', 'здоровье', 'гармония', 'психология'],
+	openGraph: {
+		type: 'website',
+		locale: 'en_IE',
+		url: 'https://beautymyway.es',
+		title: 'Beautymyway ➤ Професиональный косметолог в Валенсии',
+		description: 'Beautymyway ➤ косметолог в Валенсии⭐ Профессионально помогаю обрести красоту и здоровье! ✅ ➤ Опыт более 15 лет!',
+		siteName: 'beautymyway',
+		images: '/apple-icon.png'
+	},
+	themeColor: '#2b3632',
+};
 
 export default function RootLayout({
 	children,
+	params
 }: {
-	children: React.ReactNode
+	children: React.ReactNode;
+	params: { locale: string };
 }) {
 	return (
-		<html lang="ru">
+		<html lang={params.locale}>
 			<head>
 				<ColorSchemeScript />
 			</head>
 			<body className={`${mulish.className} ${futura.variable} text-primary bg-foreground`}>
+
 				<MantineProvider theme={theme}>
-					<div className='wrapper flex flex-col min-h-screen'>
-						<Header></Header>
-						<main className="content flex-1">
-							{children}
-						</main>
-						<Footer></Footer>
-					</div>
+					{children}
 				</MantineProvider>
 			</body>
-		</html>
+		</html >
 	)
 }

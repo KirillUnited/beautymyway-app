cfc96489223560339e1bd979dc40da6a3be521f1
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-05-27 14:46:39 +0300

    Localize 'HIT' section

--- src/components/Sections/HitSection/HitSection.tsx
+++ src/components/Sections/HitSection/HitSection.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import ImageSectionLayout from '@/components/ImageSectionLayout/ImageSectionLayout';
 import styles from './HitSection.module.scss';
 import { ButtonLink } from '@/components/Button/Button';
+import { useTranslations } from 'next-intl';
 
 export default function HitSection() {
+    const t = useTranslations('HIT');
     return (
         <ImageSectionLayout
             image={`featured-product-1.jpeg`}
-            title='Процедура месяца'
-            description='Фотолечение и Фотоомоложение (IPL)'
+            title={`${t('title')}`}
+            description={`${t('subtitle')}`}
             className={styles.wrapper}
         >
             <p>
-            Фотоомоложение представляет собой медицинскую процедуру, направленную на предотвращение и устранение признаков фотостарения кожи. Эффективная и неинвазивная, эта технология осуществляется с использованием высокоинтенсивных источников света (IPL), представляя собой одно из наиболее перспективных направлений в области эстетической медицины.
+                {t('description')}
             </p>
-            <ButtonLink href={'/posts/fotoomolozhenie-ipl'} variant='primary' className='self-center lg:self-start mt-4 lg:mt-auto'>Детальнее о процедуре</ButtonLink>
+            <ButtonLink href={'/posts/fotoomolozhenie-ipl'} variant='primary' className='self-center lg:self-start mt-4 lg:mt-auto'>{t('CTA title')}</ButtonLink>
         </ImageSectionLayout>
     )
 }

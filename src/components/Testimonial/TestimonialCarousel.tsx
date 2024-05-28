1955ca2a9217212bec5fb9a7076ed3c2388b4c48
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-05-28 11:09:55 +0300

    Localize 'TESTIMONIALS' section

--- src/components/Testimonial/TestimonialCarousel.tsx
+++ src/components/Testimonial/TestimonialCarousel.tsx
@@ -9,17 +9,20 @@ import { useMediaQuery } from '@mantine/hooks';
 import styles from './styles.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { useFormatter, useTranslations } from 'next-intl';
 
 export default function TestimonialCarousel() {
+    const t = useTranslations('TESTIMONIALS.list');
+    const format = useFormatter()
     const theme = useMantineTheme();
     const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.md})`)
     const autoplay = useRef(Autoplay({ delay: 3000 }));
-    const items = TESTIMONIALS.map(({ image, title, subtitle, description }, index) => {
+    const items = TESTIMONIALS.map((item, index) => {
         return (
             <Carousel.Slide key={index}>
                 <p className='font-light italic mb-10 px-0 lg:px-12 lg:mb-0 text-center'>
                     <FontAwesomeIcon icon={faQuoteLeft} className='mr-4 align-bottom' size='2x' />
-                    {description}
+                    {t(`${index + 1}.description`)}
                     <FontAwesomeIcon icon={faQuoteRight} className='ml-4 align-middle' size='2x' />
                 </p>
             </Carousel.Slide>

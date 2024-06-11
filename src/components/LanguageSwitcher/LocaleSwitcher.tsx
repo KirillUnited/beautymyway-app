15d5bd727cf17f20dab3ce44ef3b6df60d8f8b16
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-06-11 12:52:56 +0300

    Add "@tabler/icons-react"

--- src/components/LanguageSwitcher/LocaleSwitcher.tsx
+++ src/components/LanguageSwitcher/LocaleSwitcher.tsx
@@ -6,6 +6,7 @@ import {
     type Locale,
 } from "@/i18n.config";
 import { Select } from "@mantine/core";
+import { IconLanguageHiragana } from '@tabler/icons-react';
 
 export default function LocaleSwitcher({
     locale,
@@ -17,15 +18,19 @@ export default function LocaleSwitcher({
     return (
         <div>
             <Select
+                variant="unstyled"
                 onChange={(newLocale) => changeLocale(newLocale)}
                 checkIconPosition="left"
+                rightSection={<IconLanguageHiragana/>}
                 data={[...locales]}
                 defaultValue={locale}
                 comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
                 classNames={
                     {
-                        input: 'uppercase bg-transparent text-primary border-none w-20',
-                        option: 'text-foreground uppercase'
+                        wrapper: 'inline-flex',
+                        input: 'uppercase text-primary flex-0 w-16',
+                        option: 'text-foreground uppercase',
+                        dropdown: 'min-w-[80px]'
                     }
                 }
             />

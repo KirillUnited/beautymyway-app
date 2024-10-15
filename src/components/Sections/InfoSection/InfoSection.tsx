60a677f3c4746b1f5a19cabd9ed782d6d529365e
dubov <Kirill.Dubov@enterra-inc.com>
2024-10-15 18:31:35 +0300

    feat('Info' section): :sparkles: Add Google map

--- src/components/Sections/InfoSection/InfoSection.tsx
+++ src/components/Sections/InfoSection/InfoSection.tsx
@@ -3,15 +3,32 @@ import Contacts from '@/components/Contacts/Contacts';
 import styles from './InfoSection.module.scss';
 import { CONTACTS } from '@/data';
 import { useTranslations } from 'next-intl';
+import { GoogleMapsEmbed } from '@next/third-parties/google'
+import classNames from 'classnames';
 
 export default function InfoSection() {
     const t = useTranslations('Info');
 
     return (
         <section id='info' className={styles.root}>
-            <div className={styles.wrapper}>
-                <p className={styles.description}>{t('description')}</p>
-                <Contacts className={styles.contacts} tel={CONTACTS.tel} address={CONTACTS.adress} time={CONTACTS.time} email={CONTACTS.email} />
+            <div className="container">
+                <div className={classNames(styles.wrapper)}>
+                    <div className='section-heading'>
+                        <h2 className='section-title'>{t('title')}</h2>
+                    </div>
+                    <p className={styles.description}>{t('description')}</p>
+                    <Contacts className={styles.contacts} tel={CONTACTS.tel} address={CONTACTS.adress} time={CONTACTS.time} email={CONTACTS.email} />
+                    <div className='w-full bg-slate-300'>
+                        <GoogleMapsEmbed
+                            apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}
+                            width="100%"
+                            height={500}
+                            mode="place"
+                            q="Brooklyn+Bridge,New+York,NY"
+                            language='es'
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     )
